2c1a9c9fe5f6f1b722774044262463029c0d84ef
andantet <orrdaniel73@gmail.com>
2022-09-16 08:14:41 +0100

    datagen innit
    (someone else needs to implement the actual meat of the datagen, I've gotta go school)

---
+++ fabric/src/datagen/java/com/github/teamfusion/rottencreatures/datagen/RottenCreaturesDataGenerator.java
@@ -0,0 +1,10 @@
+package com.github.teamfusion.rottencreatures.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class RottenCreaturesDataGenerator implements DataGeneratorEntrypoint {
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator gen) {
+    }
+}
