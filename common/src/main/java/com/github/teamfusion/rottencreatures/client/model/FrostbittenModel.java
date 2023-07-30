206c39e27d27d756f23f6cec756b5181b8f399d4
ItsBlackGear <itsblackgear@gmail.com>
2023-07-30 19:48:38 -0400

    👌 - Zombie Overhaul: Frostbitten
    - Frostbitten zombies will now generate a smaller ice patch whenever they walk on water.
    - Baby Frostbitten Snow Explosion changes:
        - New visuals for the Snow Explosion.
        - Upon exploding, it will affect nearby entities in a 5 block radius instead of 10.
        - The explosion will freeze all nearby entities instead of just players (as long as they can freeze).
        - The duration of the freezing effect applied may vary depending on the difficulty.

--- common/src/main/java/com/github/teamfusion/rottencreatures/client/model/FrostbittenModel.java
+++ common/src/main/java/com/github/teamfusion/rottencreatures/client/model/FrostbittenModel.java
@@ -24,9 +24,4 @@ public class FrostbittenModel<T extends Zombie> extends ZombieModel<T> {
         root.addOrReplaceChild("left_arm", CubeListBuilder.create().texOffs(32, 48).addBox(-2.0F, -2.0F, -2.0F, 4.0F, 12.0F, 4.0F, new CubeDeformation(0.0F)).texOffs(64, 48).addBox(-2.0F, -2.0F, 2.0F, 4.0F, 12.0F, 4.0F, new CubeDeformation(0.0F)).texOffs(48, 48).addBox(-2.0F, -2.0F, -2.0F, 4.0F, 12.0F, 4.0F, new CubeDeformation(0.25F)), PartPose.offsetAndRotation(6.0F, -10.0F, 0.0F, -1.5708F, 0.0F, 0.0F));
         return LayerDefinition.create(mesh, 96, 64);
     }
-
-    @Override
-    public boolean isAggressive(T zombie) {
-        return zombie.isAggressive();
-    }
 }
--- common/src/main/java/com/github/teamfusion/rottencreatures/common/entities/Frostbitten.java
+++ common/src/main/java/com/github/teamfusion/rottencreatures/common/entities/Frostbitten.java
@@ -4,13 +4,11 @@ import com.github.teamfusion.rottencreatures.client.registries.RCSoundEvents;
 import com.github.teamfusion.rottencreatures.common.registries.RCMobEffects;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.particles.ParticleTypes;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntitySelector;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.MobSpawnType;
@@ -27,6 +25,9 @@ import java.util.List;
 import java.util.Random;
 
 public class Frostbitten extends Zombie {
+    // Entity Events
+    private static final byte SNOW_EXPLOSION_ID = 14;
+
     public Frostbitten(EntityType<? extends Zombie> type, Level level) {
         super(type, level);
         this.xpReward = 5;
@@ -34,21 +35,30 @@ public class Frostbitten extends Zombie {
         this.setPathfindingMalus(BlockPathTypes.DANGER_POWDER_SNOW, -1.0F);
     }
 
-    public static AttributeSupplier.Builder createAttributes() {
-        return Zombie.createAttributes().add(Attributes.SPAWN_REINFORCEMENTS_CHANCE, 0.0D).add(Attributes.MAX_HEALTH, 20.0D).add(Attributes.MOVEMENT_SPEED, 0.2D).add(Attributes.ATTACK_DAMAGE, 3.0D).add(Attributes.ARMOR, 2.0D);
-    }
-
     @Override
     protected void addBehaviourGoals() {
         super.addBehaviourGoals();
+        // Frostbitten Zombies and Burned Zombies are enemies and will attack each other if possible.
         this.targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(this, Burned.class, true));
+    }
+
+    public static AttributeSupplier.Builder createAttributes() {
+        return Zombie.createAttributes()
+                .add(Attributes.SPAWN_REINFORCEMENTS_CHANCE, 0.0D)
+                .add(Attributes.MAX_HEALTH, 20.0D)
+                .add(Attributes.MOVEMENT_SPEED, 0.2D)
+                .add(Attributes.ATTACK_DAMAGE, 3.0D)
+                .add(Attributes.ARMOR, 2.0D);
     }
 
     @Override
     public boolean doHurtTarget(Entity entity) {
         boolean hurt = super.doHurtTarget(entity);
-        if (hurt && this.getMainHandItem().isEmpty() && entity instanceof LivingEntity living) {
-            if (living.canFreeze()) {
+
+        // Check if the zombie can hurt the target, and it's not holding any item.
+        if (hurt && this.getMainHandItem().isEmpty()) {
+            // Check if the entity can freeze, then apply the freezing effect, the duration may vary depending on the difficulty.
+            if (entity instanceof LivingEntity living && living.canFreeze()) {
                 float modifier = this.level.getCurrentDifficultyAt(this.blockPosition()).getEffectiveDifficulty();
                 living.addEffect(new MobEffectInstance(RCMobEffects.FREEZE.get(), 140 * (int)modifier), this);
             }
@@ -60,7 +70,8 @@ public class Frostbitten extends Zombie {
     @Override
     protected void onChangedBlock(BlockPos pos) {
         super.onChangedBlock(pos);
-        FrostWalkerEnchantment.onEntityMoved(this, this.level, pos, 2);
+        // Once a Frostbitten walks nearby water, it freezes it temporally.
+        FrostWalkerEnchantment.onEntityMoved(this, this.level, pos, 0);
     }
 
     @Override
@@ -81,22 +92,69 @@ public class Frostbitten extends Zombie {
     @Override
     public void die(DamageSource source) {
         if (this.isBaby()) {
-            for (int i = 0; i < 8; i++) {
-                this.level.addParticle(ParticleTypes.CLOUD, this.getX(), this.getRandomY(), this.getZ(), -0.15D + this.random.nextDouble(0.45D), 0.0D, -0.15D + this.random.nextDouble(0.45D));
-            }
+            // Apply a Snow Explosion once a baby died.
+            this.level.broadcastEntityEvent(this, SNOW_EXPLOSION_ID);
 
             if (!this.level.isClientSide) {
-                MobEffect freeze = RCMobEffects.FREEZE.get();
-                List<ServerPlayer> players = ((ServerLevel)this.level).getPlayers(player -> this.distanceToSqr(player) < 10.0F && player.gameMode.isSurvival());
-                for (ServerPlayer player : players) {
-                    if (!player.hasEffect(freeze)) {
-                        player.addEffect(new MobEffectInstance(freeze, 100), this);
+                // Check for any nearby entities in a radius of 5 blocks from the Frostbitten
+                List<LivingEntity> entities = this.level.getEntitiesOfClass(
+                        LivingEntity.class,
+                        this.getBoundingBox().inflate(5.0D),
+                        EntitySelector.NO_CREATIVE_OR_SPECTATOR
+                );
+
+                for (LivingEntity entity : entities) {
+                    // Check if the entity can freeze, and apply the Freezing effect, the durability may vary depending on the difficulty.
+                    if (entity.canFreeze()) {
+                        float modifier = this.level.getCurrentDifficultyAt(this.blockPosition()).getEffectiveDifficulty();
+                        entity.addEffect(new MobEffectInstance(RCMobEffects.FREEZE.get(), 100 * (int)modifier), this);
                     }
                 }
             }
         }
 
         super.die(source);
+    }
+
+    @Override
+    public void handleEntityEvent(byte id) {
+        super.handleEntityEvent(id);
+        if (id == SNOW_EXPLOSION_ID) {
+            this.createSnowExplosion();
+        }
+    }
+
+    private void createSnowExplosion() {
+        double posX = this.getX();
+        double posY = this.getY();
+        double posZ = this.getZ();
+
+        for (int i = 0; i < 100; i++) {
+            // Calculate random values for motion in each direction
+            double motionX = this.random.nextGaussian() * 0.2D;
+            double motionY = this.random.nextGaussian() * 0.2D;
+            double motionZ = this.random.nextGaussian() * 0.2D;
+
+            // Spawn particles in a spherical pattern
+            for (int j = 0; j < 20; j++) {
+                double xzAngle = this.random.nextDouble() * Math.PI * 2.0D;
+                double yAngle = this.random.nextDouble() * Math.PI / 2.0D;
+
+                double motionXDir = Math.cos(xzAngle) * Math.sin(yAngle);
+                double motionYDir = Math.cos(yAngle);
+                double motionZDir = Math.sin(xzAngle) * Math.sin(yAngle);
+
+                this.level.addParticle(
+                        ParticleTypes.SNOWFLAKE,
+                        posX,
+                        posY,
+                        posZ,
+                        motionX + motionXDir * 0.1D,
+                        motionY + motionYDir * 0.1D,
+                        motionZ + motionZDir * 0.1D
+                );
+            }
+        }
     }
 
     public static boolean checkFrostbittenSpawnRules(EntityType<Frostbitten> type, ServerLevelAccessor level, MobSpawnType spawnType, BlockPos pos, Random random) {
